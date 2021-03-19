f153a2ef2f73eb17a0d5a7d49d62476ca1da5964
Armando Miani <armando.miani@gmail.com>
2021-03-19 20:18:57 +0100

    Add tests

--- lambda/test/validators_test.js
+++ lambda/test/validators_test.js
@@ -4,13 +4,17 @@ const validators = require('../validators')
 
 describe('Validators', () => {
   
+  var validator = new validators.Country('pt-br');
+
   it('should find a Country', () => {
-    var validator = new validators.Country('pt-br');
     assert.strictEqual(validator.validate('brasil'), true);
   });
 
+  it('should not find a Country', () => {
+    assert.strictEqual(validator.validate('uniao sovietica'), false);
+  });
+
   it('should not find a Color', () => {
-    var validator = new validators.Color('pt-br');
     assert.strictEqual(validator.validate('jose'), false);
   });
 });
