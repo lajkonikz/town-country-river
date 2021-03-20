63a8f2cb9e7a30acbcbedc5bfb85f86484326eb2
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-03-20 00:30:53 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -20,11 +20,11 @@ const HowManyPlayersIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HowManyPlayersIntent';
     },
     handle(handlerInput) {
-        //let numberOfPlayers = this.event.request.intent.slots.numberOfPlayers.value;
+        let numberOfPlayers = handlerInput.resuestEnvelope.request.intent.slots.numberOfPlayers.value;
         const speakOutput = 'test';
 
         return handlerInput.responseBuilder
-            .speak(speakOutput)
+            .speak(numberOfPlayers)
             .reprompt(speakOutput)
             .getResponse();
     }
